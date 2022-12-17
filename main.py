8cf085b16c3fabcf92b1a0d82ee5d27aaadf00ed
Muhammadreza Haghiri <haghiri75@gmail.com>
2022-12-17 20:25:43 +0000

    7B and 3B models were to huge for my GPU, let's try 1B

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
 import torch
 torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-7b1", use_cache=True)
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-7b1")
+model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b3", use_cache=True)
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b3")
